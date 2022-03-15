655553518b91e8dcc8164b79594d8b2736446386
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-03-15 08:45:44 +0100

    dbsearch no results found

--- src/components/replaceTemplateParts/dbSearch/index.js
+++ src/components/replaceTemplateParts/dbSearch/index.js
@@ -3,23 +3,26 @@ import mdCardHTML from './mdCard.html'
 class EditDbSearshTextcomponentController {
   constructor($scope, $element, $compile) {
     //this is a watcher on the prm-search component and will trigger replacement of content
-    let componentWatcher = $scope.$watch(() => {
-      let prmDatabaseMdCardExists =  $element.parent().parent().find('prm-search').find('md-card').length > 0;
-      return prmDatabaseMdCardExists
-    }, (n, o) => {
-      if (n == true) {
-        if ( $element.parent().parent().find('prm-search').find('md-card').length > 0 ) {
-          let mdCardElement = $element.parent().parent().find('prm-search').find('md-card');
-          var htmlResult = $compile(mdCardHTML)( $scope.$root  );
-          mdCardElement.empty()
-          mdCardElement.append( htmlResult );
-          componentWatcher(); //deregister watcher
+    //console.log( $scope.$root.$$childHead.$ctrl.$state.params.databases );
+    if ($scope.$root.$$childHead.$ctrl.$state.params.databases == undefined) {
+      let componentWatcher = $scope.$watch(() => {
+        let prmDatabaseMdCardExists =  $element.parent().parent().find('prm-search').find('md-card').length > 0;
+        return prmDatabaseMdCardExists
+      }, (n, o) => {
+        if (n == true) {
+          if ( $element.parent().parent().find('prm-search').find('md-card').length > 0 ) {
+            let mdCardElement = $element.parent().parent().find('prm-search').find('md-card');
+            var htmlResult = $compile(mdCardHTML)( $scope.$root  );
+            mdCardElement.empty()
+            mdCardElement.append( htmlResult );
+            componentWatcher(); //deregister watcher
+          }
         }
-      }
-    }, false);
-
+      }, false);
+    }
   }
 }
+
 
 EditDbSearshTextcomponentController.$inject = ['$scope', '$element', '$compile'];
 
